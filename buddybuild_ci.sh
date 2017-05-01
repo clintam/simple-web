9f701bfa82a01d64e041df6ccff3bc32221f8c86
Clint Morgan <clint@buddybuild.com>
2017-05-01 12:25:51 -0700

    avoid clash

--- buddybuild_ci.sh
+++ buddybuild_ci.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 echo "Running custom build script"
-git clone git@github.com:clintam/simple-web.git
-git clone git@github.com:BuddyBuild/simple-web.git
+git clone git@github.com:clintam/simple-web.git sw1
+git clone git@github.com:BuddyBuild/simple-web.git sw2
