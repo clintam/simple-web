eb09484795a59730bbe0ad7d0887cb900f0be61f
Clint Morgan <clint@buddybuild.com>
2017-05-08 09:19:22 -0700

    does not require other repo

--- buddybuild_ci.sh
+++ buddybuild_ci.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 echo "Running custom build script"
-git clone git@github.com:clintam/simple-web.git sw1
-git clone git@github.com:BuddyBuild/simple-web.git sw2
+#git clone git@github.com:clintam/simple-web.git sw1
+#git clone git@github.com:BuddyBuild/simple-web.git sw2
